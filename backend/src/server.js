f41e1a24b8765b9d5bfc9a75db8de41a452c6b6d
Mahender Yadav <mahenderydv3@gmail.com>
2026-08-08 20:27:07 +0530

    correct data

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { createApp } from "./app.js";
 import { connectDB } from "./config/db.js";
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 const app = createApp();
 
 connectDB().then(() => {
